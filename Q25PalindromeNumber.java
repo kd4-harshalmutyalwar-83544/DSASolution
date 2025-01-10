2b7fc5e17cc9198a43551a1be37c6b2048c15aaf
Harshal Mutyalwar <harshalmutyalwar@gmail.com>
2025-01-10 23:02:43 +0530

    Palindrome Number

---
+++ Q25PalindromeNumber.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+public class Q25PalindromeNumber
+{
+  public static void main(String args[])
+  {
+     Scanner sc = new Scanner(System.in);
+
+     System.out.println("Enter the value of n:");
+     int n = sc.nextInt();
+
+     int temp = n;
+     int rev = 0;
+     
+     while(n!=0)
+     {
+         rev = rev*10 + (n%10);
+         n = n/10;
+     }
+
+     if(temp == rev)
+     {
+        System.out.println(temp +" is a Palindrome");
+     }
+     else
+     {
+        System.out.println(temp + " is not palindrome");
+     }
+
+  }  
+}
