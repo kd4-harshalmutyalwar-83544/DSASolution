335f3651a136a9025145df9abee15aa2b6e1bd2b
Harshal Mutyalwar <harshalmutyalwar@gmail.com>
2025-01-22 20:04:54 +0530

    java Pattern question

---
+++ Q65Pattern.java
@@ -0,0 +1,38 @@
+/*
+              *           *
+                 *     *
+                    *
+                 *     *
+              *           *
+              for n=5
+
+*/
+
+import java.util.Scanner;
+
+public class Q65Pattern 
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the value of n");
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=n; j++)
+            {
+                if(j==i || j==n+1-i )
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
